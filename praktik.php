f799c0a796e13891707a6d33c2ea077435fefa42
unknown <holidmuhamadsalman@gmail.com>
2023-05-03 19:30:49 +0700

    praktik individu 1

---
+++ praktik.php
@@ -0,0 +1,10 @@
+<?php
+    for($no=1;$no<=10;$no++){
+        if($no % 2 == 0){
+            echo "$no ";echo "ini adalah bilangan genap<br>";
+        }
+        else{
+            echo "$no ";echo "Ini adalah bilangan ganjil<br>";
+        }
+    }
+?>
